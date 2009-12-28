5557e73a4ad69d6440badfacd16714e5998e8a60
mauricio.silva <mauricio.silva@gmail.com>
2009-12-27 22:55:19 -0800

    added removal of unneeded files and curled jquery and mu.js

--- bort.rb
+++ bort.rb
@@ -6,6 +6,19 @@ inside('vendor') do
   run "ln -s ~/commit-rails/rails rails"
 end
 
+# remove unnesesary files
+run "rm README"
+run "rm public/index.html"
+run "rm public/favicon.ico"
+run "rm public/robots.txt"
+run 'rm -f public/javascripts/*'
+
+# get jquery
+run 'curl -L http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js > public/javascripts/jquery.min.js'
+# get MU
+run 'curl -L http://static.ak.fbcdn.net/connect/en_US/core.js > public/javascripts/mu.js'
+
+=begin
 plugin 'rspec', 
   :git => 'git://github.com/dchelimsky/rspec.git'
 plugin 'rspec-rails', 
@@ -16,16 +29,17 @@ plugin 'asset_packager',
   :git => 'http://synthesis.sbecker.net/pages/asset_packager'
 plugin 'role_requirement', 
   :git => 'git://github.com/timcharper/role_requirement.git'
-plugin 'restful-authentication', 
-  :git => 'git://github.com/technoweenie/restful-authentication.git'
- 
 gem 'mislav-will_paginate', :version => '~> 2.2.3', 
   :lib => 'will_paginate',  :source => 'http://gems.github.com'
+ 
+=end
+plugin 'restful-authentication', 
+  :git => 'git://github.com/technoweenie/restful-authentication.git'
  
 rake("gems:install", :sudo => true)
 
 generate("authenticated", "user session")
-generate("rspec")
+#generate("rspec")
 
 
 puts "all done."
